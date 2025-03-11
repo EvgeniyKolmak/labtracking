c76b45377cc2120cd411c241838176bddadd8991
Evgeniy Kolmak <zenakolmak669@gmail.com>
2025-03-11 16:21:23 +0300

    init commit

---
+++ src/main/java/com/example/labtracking/LabtrackingApplication.java
@@ -0,0 +1,13 @@
+package com.example.labtracking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabtrackingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LabtrackingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/labtracking/LabtrackingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.labtracking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabtrackingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
